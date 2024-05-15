c39b3306cfb6494a3841d9366291daff451176d0
MohamedIjlal2000 <mohamedijlal27@gmail.com>
2024-05-15 14:57:31 +0530

    First Commit

---
+++ src/components/bodyComponents/customer/Customers.js
@@ -0,0 +1,190 @@
+import productList from "../inventory/productList";
+
+const customers = [{
+    id:1,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:2,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:3,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:4,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:5,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:6,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:7,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:8,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:9,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:10,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:11,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:12,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:13,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:14,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:15,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:16,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:17,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:18,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:19,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:20,firstName: "Mohamed",
+    lastName: "Ijlal",
+    address: "Colombo",
+    mobile: "0727920628",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}];
+export default customers;
